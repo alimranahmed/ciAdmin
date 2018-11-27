a38b2a7cf581d0339aabd0ef54ba5c20828beb26
Al Imran Ahmed <al.imran.cse@gmail.com>
2018-11-27 14:56:27 +0600

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,5 +1,11 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     $(".sidebar .has-children").click(function () {
-        $(this).parent().find('ul').toggle();
+        $(this).parent().find('ul').toggle(150);
     });
+
+    let hash = window.location.hash;
+
+    if (hash) {
+        $(hash).parent().find('ul').css("display", "block");
+    }
 });
